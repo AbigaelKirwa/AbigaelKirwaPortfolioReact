37da079e1a877daf8019a71a2ebb916f6cbe6de3
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-01-28 20:27:33 +0300

    feat(skills):added skills page to components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './components/Home';
 import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <Home/>
       <Projects/>
+      <Skills/>
     </div>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,79 @@
 function Skills() {
     return(
-        <div>
-        
-        </div>
+        <section id="skills">
+            {/* this is code for the skills section */}
+            <div class="skills_heading">
+                <h1>SKILLS</h1>
+            </div>
+            <div class="numerical_skills">
+                <div class="skill" id="skill-one">
+                    <h1 class="count" data-val="10">00</h1>
+                    <p>Happy Clients</p>
+                </div>
+                <div class="skill" id="skill-two">
+                    <h1 class="count" data-val="6">00</h1>
+                    <p>Projects</p>
+                </div>
+                <div class="skill" id="skill-three">
+                    <h1 class="count" data-val="5">00</h1>
+                    <p>Awards</p>
+                </div>
+                <div class="skill" id="skill-four">
+                    <h1 class="count" data-val="8">00</h1>
+                    <p>Languages</p>
+                </div>
+            </div>
+            <div class="languages">
+                <div class="language">
+                    <p>HTML</p>
+                    <div class="language_stats">
+                        <div class="overall">
+                            <div class="current" id="html"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="language" data-percentage="90">
+                    <p>CSS</p>
+                    <div class="language_stats">
+                        <div class="overall">
+                            <div class="current" id="css"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="language" data-percentage="50">
+                    <p>JavaScript</p>
+                    <div class="language_stats">
+                        <div class="overall">
+                            <div class="current" id="js"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="language" data-percentage="50">
+                    <p>React</p>
+                    <div class="language_stats">
+                        <div class="overall">
+                            <div class="current" id="react"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="language" data-percentage="70">
+                    <p>PHP</p>
+                    <div class="language_stats">
+                        <div class="overall">
+                            <div class="current" id="php"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="language" data-percentage="60">
+                    <p>Python</p>
+                    <div class="language_stats">
+                        <div class="overall">
+                            <div class="current" id="python"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
