6bbd32e885dbc16d4de1f63189055f18f457a98f
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-01-28 21:12:38 +0300

    fix(skills):added animation for loading elements using useEffect and useState hooks

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,20 +7,20 @@ function Projects() {
     return(
         <section id="projects">
             {/* this is code for the projects section */}
-            <div class="projects_heading">
+            <div className="projects_heading">
                 <h1>PROJECTS</h1>
             </div>
-            <div class="main_images_projects">
-                <div class="image_div">
+            <div className="main_images_projects">
+                <div className="image_div">
                     <a href="https://kirche.netlify.app/" target="_blank"><img src={Kirche} alt="kirche project"/></a>
                 </div>
-                <div class="image_div">
+                <div className="image_div">
                     <a href="https://computerco.netlify.app/" target="_blank"><img src={ComputerCo} alt="computer project"/></a>
                 </div>
-                <div class="image_div">
+                <div className="image_div">
                     <a href="https://abigaelkirwa.github.io/Animation-Website/" target="_blank"><img src={Animation} alt="animation project"/></a>
                 </div>
-                <div class="image_div">
+                <div className="image_div">
                     <a href="https://afyaweb.netlify.app/" target="_blank"><img src={Afya} alt="afya project"/></a>
                 </div>
             </div>
